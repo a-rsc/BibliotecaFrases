4c422a68286e4e1911d1e19113b2c2502835b084
Alvaro Rodriguez <a_rsc@hotmail.com>
2021-01-20 13:06:11 +0100

    first commit

---
+++ BibliotecaFrases/Biblioteca.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaFrases
+{
+    public class Biblioteca
+    {
+        //public List<string> paraulesRepetides(string frase1, string frase2)
+        //{
+        //    List<string> paraulesFrase1 = new List<string>(frase1.Split(' '));
+        //    List<string> paraulesFrase2 = new List<string>(frase2.Split(' '));
+        //    List<string> paraulesRepetides = new List<string>();
+
+        //    foreach(string paraula1 in paraulesFrase1)
+        //    {
+        //        if(paraulesFrase2.Contains(paraula1))
+        //        {
+        //            paraulesRepetides.Add(paraula1);
+        //        }
+        //    }
+
+        //    return paraulesRepetides;
+        //}
+
+        public List<string> paraulesRepetides(string frase1, string frase2)
+        {
+            Hashtable frase = new Hashtable();
+
+            List<string> paraulesRepetides = new List<string>();
+            List<string> paraulesFrase1;
+            List<string> paraulesFrase2;
+
+            if(
+                !String.IsNullOrEmpty(frase1) &&
+                !String.IsNullOrEmpty(frase2))
+            {
+                paraulesFrase1 = new List<string>(frase1.Split(' '));
+                paraulesFrase2 = new List<string>(frase2.Split(' '));
+
+                foreach(string paraula1 in paraulesFrase1)
+                {
+                    frase.Add(paraula1, null);
+                }
+
+                foreach(string paraula2 in paraulesFrase2)
+                {
+                    if(frase.ContainsKey(paraula2))
+                    {
+                        paraulesRepetides.Add(paraula2);
+                    }
+                }
+            }
+
+            return paraulesRepetides;
+        }
+
+        public List<string> paraulesNoRepetides(string frase1, string frase2)
+        {
+            Hashtable frase = new Hashtable();
+
+            List<string> paraulesNoRepetides = new List<string>();
+            List<string> paraulesFrase1;
+            List<string> paraulesFrase2;
+
+            if(
+                !String.IsNullOrEmpty(frase1)&&
+                !String.IsNullOrEmpty(frase2))
+            {
+
+                paraulesFrase1 = new List<string>(frase1.Split(' '));
+                paraulesFrase2 = new List<string>(frase2.Split(' '));
+
+                foreach(string paraula1 in paraulesFrase1)
+                {
+                    if(!frase.ContainsKey(paraula1))
+                    {
+                        frase.Add(paraula1, null);
+                    }
+                }
+
+                foreach(string paraula2 in paraulesFrase2)
+                {
+                    if(!frase.ContainsKey(paraula2))
+                    {
+                        frase.Add(paraula2, null);
+                    }
+                    else
+                    {
+                        frase.Remove(paraula2);
+                    }
+                }
+
+                foreach(DictionaryEntry f in frase)
+                {
+                    paraulesNoRepetides.Add(f.Key.ToString());
+                }
+
+            } else if(String.IsNullOrEmpty(frase2))
+            {
+                paraulesFrase1=new List<string>(frase1.Split(' '));
+
+                foreach(string paraula1 in paraulesFrase1)
+                {
+                    if(!frase.ContainsKey(paraula1))
+                    {
+                        frase.Add(paraula1, null);
+                    }
+                }
+
+                foreach(DictionaryEntry f in frase)
+                {
+                    paraulesNoRepetides.Add(f.Key.ToString());
+                }
+
+            }
+            else
+            {
+                paraulesFrase2=new List<string>(frase2.Split(' '));
+
+                foreach(string paraula2 in paraulesFrase2)
+                {
+                    if(!frase.ContainsKey(paraula2))
+                    {
+                        frase.Add(paraula2, null);
+                    }
+                }
+
+                foreach(DictionaryEntry f in frase)
+                {
+                    paraulesNoRepetides.Add(f.Key.ToString());
+                }
+            }
+
+            return paraulesNoRepetides;
+        }
+
+        public List<string> paraulesMesRepetides(string frase1, string frase2)
+        {
+            Hashtable frase = new Hashtable();
+
+            List<string> paraulesMesRepetides = new List<string>();
+            List<string> paraulesFrase1;
+            List<string> paraulesFrase2;
+
+            if(
+                !String.IsNullOrEmpty(frase1)&&
+                !String.IsNullOrEmpty(frase2))
+            {
+                paraulesFrase1=new List<string>(frase1.Split(' '));
+                paraulesFrase2=new List<string>(frase2.Split(' '));
+
+                foreach(string paraula1 in paraulesFrase1)
+                {
+                    if(frase.ContainsKey(paraula1))
+                    {
+                        frase[paraula1] = Convert.ToString(int.Parse(frase[paraula1].ToString())+1);
+                    }
+                    else
+                    {
+                        frase.Add(paraula1, "0");
+                    }
+                }
+
+                foreach(string paraula2 in paraulesFrase2)
+                {
+                    if(frase.ContainsKey(paraula2))
+                    {
+                        frase[paraula2]=Convert.ToString(int.Parse(frase[paraula2].ToString())+1);
+                    }
+                    else
+                    {
+                        frase.Add(paraula2, "0");
+                    }
+                }
+
+                frase.Cast<DictionaryEntry>().OrderBy(entry => entry.Value).ToList();
+
+                foreach(DictionaryEntry f in frase)
+                {
+                    if(f.Value.ToString() != "0")
+                    {
+                        paraulesMesRepetides.Add(f.Key.ToString());
+                    }
+                }
+            }
+
+            return paraulesMesRepetides;
+        }
+
+        public List<string> ParaulesRepetidesFile(ref StreamReader f1, ref StreamReader f2)
+        {
+            List<string> paraulesRepetides = new List<string>();
+
+            string frase1;
+            string frase2;
+
+            frase1=ParaulesFile(ref f1);
+            frase2=ParaulesFile(ref f2);
+
+            paraulesRepetides=this.paraulesRepetides(frase1, frase2);
+
+            return paraulesRepetides;
+        }
+
+        public List<string> ParaulesNoRepetidesFile(ref StreamReader f1, ref StreamReader f2)
+        {
+            List<string> paraulesNoRepetides = new List<string>();
+
+            string frase1;
+            string frase2;
+
+            frase1=ParaulesFile(ref f1);
+            frase2=ParaulesFile(ref f2);
+
+            paraulesNoRepetides=this.paraulesNoRepetides(frase1, frase2);
+
+            return paraulesNoRepetides;
+        }
+
+        public List<string> paraulesMesRepetidesFile(ref StreamReader f1, ref StreamReader f2)
+        {
+            List<string> paraulesMesRepetides = new List<string>();
+
+            string frase1;
+            string frase2;
+
+            frase1=ParaulesFile(ref f1);
+            frase2=ParaulesFile(ref f2);
+
+            paraulesMesRepetides=this.paraulesMesRepetides(frase1, frase2);
+
+            return paraulesMesRepetides;
+        }
+
+        public String ParaulesFile(ref StreamReader f)
+        {
+            string paraula, frase = "";
+
+            try
+            {
+                while(!f.EndOfStream)
+                {
+                    paraula=f.ReadLine();
+                    if(paraula.Trim()!="")
+                    {
+                        frase += paraula + ' ';
+                    }
+                }
+
+                frase = frase.Trim();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+
+            return frase;
+        }
+    }
+}
---
+++ PruebasUnitarias/UnitTest1.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BibliotecaFrases;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Biblioteca biblioteca;
+        List<string> llResultat;
+        string frase1, frase2, filename1, pathfilename1, filename2, pathfilename2;
+
+        // Álvaro: Se ejecuta antes de cualquier prueba e inicializa las propiedades que se utilizan en todos los tests
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            biblioteca=new Biblioteca();
+            llResultat=new List<string>();
+        }
+
+        [TestMethod]
+        public void paraulesRepetidesTest()
+        {
+            frase1="Lorem ipsum dolor sit amet consectetur adipiscing elit";
+            frase2="ipsum dolor sit hola consectetur elit at conubia fringilla convallis feugiat sem habitant iaculis ac sollicitudin potenti pellentesque mauris";
+
+            string[] resultat = { "ipsum", "dolor", "sit", "consectetur", "elit" };
+
+            llResultat.AddRange(resultat);
+
+            CollectionAssert.AreEqual(llResultat, biblioteca.paraulesRepetides(frase1, frase2));
+        }
+
+        [TestMethod]
+        public void paraulesRepetidesNullTest()
+        {
+            frase1="Lorem ipsum dolor sit amet consectetur adipiscing elit.";
+            frase2=null;
+
+            string[] resultat = null;
+
+            if(resultat!=null)
+            {
+                llResultat.AddRange(resultat);
+            }
+
+            CollectionAssert.AreEqual(llResultat, biblioteca.paraulesRepetides(frase1, frase2));
+        }
+
+        [TestMethod]
+        public void paraulesNoRepetidesTest()
+        {
+            frase1="Lorem ipsum dolor sit amet consectetur adipiscing elit";
+            frase2="ipsum dolor sit hola consectetur elit at conubia fringilla convallis feugiat sem habitant iaculis ac sollicitudin potenti pellentesque mauris";
+
+            string[] resultat = new string[] { "Lorem", "amet", "adipiscing", "hola", "at", "conubia", "fringilla", "convallis", "feugiat", "sem", "habitant", "iaculis", "ac", "sollicitudin", "potenti", "pellentesque", "mauris" };
+
+            llResultat.AddRange(resultat);
+
+            CollectionAssert.AreEquivalent(llResultat, biblioteca.paraulesNoRepetides(frase1, frase2));
+        }
+
+        [TestMethod]
+        public void paraulesNoRepetidesNullTest()
+        {
+            //string frase1 = "Lorem ipsum dolor sit amet consectetur adipiscing elit";
+            //string frase2 = null;
+
+            //string[] resultat = new string[] { "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit" };
+
+            frase1=null;
+            frase2="ipsum dolor sit hola consectetur elit at conubia fringilla convallis feugiat sem habitant iaculis ac sollicitudin potenti pellentesque mauris";
+
+            string[] resultat = new string[] { "ipsum", "dolor", "sit", "hola", "consectetur", "elit", "at", "conubia", "fringilla", "convallis", "feugiat", "sem", "habitant", "iaculis", "ac", "sollicitudin", "potenti", "pellentesque", "mauris" };
+
+            llResultat.AddRange(resultat);
+
+            CollectionAssert.AreEquivalent(llResultat, biblioteca.paraulesNoRepetides(frase1, frase2));
+        }
+
+        [TestMethod()]
+        public void paraulesMesRepetidesTest()
+        {
+            frase1="Lorem ipsum dolor sit amet consectetur adipiscing elit ipsum";
+            frase2="consectetur dolor adipiscing elit";
+
+            string[] resultat = new string[] { "adipiscing", "dolor", "consectetur", "elit", "ipsum" };
+
+            llResultat.AddRange(resultat);
+
+            // L'ordre de les paraules amb les mateixes repeticions és imprevissible, caldria un proces que fes aquesta ordenació
+            // L'ordre de les úniques sí que es respecte
+            CollectionAssert.AreEquivalent(llResultat, biblioteca.paraulesMesRepetides(frase1, frase2));
+        }
+
+        [TestMethod()]
+        public void paraulesMesRepetidesNullTest()
+        {
+            frase1="Lorem ipsum dolor sit amet consectetur adipiscing elit ipsum";
+            frase2=null;
+
+            string[] resultat = null;
+
+            if(resultat!=null)
+            {
+                llResultat.AddRange(resultat);
+            }
+
+            CollectionAssert.AreEqual(llResultat, biblioteca.paraulesMesRepetides(frase1, frase2));
+        }
+
+        [TestMethod()]
+        public void ParaulesRepetidesFileTest()
+        {
+            filename1="file1.txt";
+            filename2="file2.txt";
+
+            pathfilename1=Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, string.Concat(@"..\..\..\", filename1));
+            pathfilename2=Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, string.Concat(@"..\..\..\", filename2));
+
+            //Debug.WriteLine(pathFile1);
+            //Debug.WriteLine(pathFile2);
+
+            StreamReader f1 = new StreamReader(pathfilename1, System.Text.Encoding.Default);
+            StreamReader f2 = new StreamReader(pathfilename2, System.Text.Encoding.Default);
+
+            string[] resultat = new string[] { "ipsum", "dolor", "sit", "consectetur", "elit" };
+
+            llResultat.AddRange(resultat);
+
+            CollectionAssert.AreEqual(llResultat, biblioteca.ParaulesRepetidesFile(ref f1, ref f2));
+        }
+
+        [TestMethod()]
+        public void ParaulesNoRepetidesFileTest()
+        {
+            filename1="file1.txt";
+            filename2="file2.txt";
+
+            pathfilename1=Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, string.Concat(@"..\..\..\", filename1));
+            pathfilename2=Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, string.Concat(@"..\..\..\", filename2));
+
+            //Debug.WriteLine(pathFile1);
+            //Debug.WriteLine(pathFile2);
+
+            StreamReader f1 = new StreamReader(pathfilename1, System.Text.Encoding.Default);
+            StreamReader f2 = new StreamReader(pathfilename2, System.Text.Encoding.Default);
+
+            string[] resultat = new string[] { "Lorem", "amet", "adipiscing", "hola", "at", "conubia", "fringilla", "convallis", "feugiat", "sem", "habitant", "iaculis", "ac", "sollicitudin", "potenti", "pellentesque", "mauris" };
+
+            llResultat.AddRange(resultat);
+
+            CollectionAssert.AreEquivalent(llResultat, biblioteca.ParaulesNoRepetidesFile(ref f1, ref f2));
+        }
+
+        [TestMethod()]
+        public void paraulesMesRepetidesFileTest()
+        {
+            filename1="file1Repetides.txt";
+            filename2="file2Repetides.txt";
+
+            pathfilename1=Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, string.Concat(@"..\..\..\", filename1));
+            pathfilename2=Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, string.Concat(@"..\..\..\", filename2));
+
+            //Debug.WriteLine(pathFile1);
+            //Debug.WriteLine(pathFile2);
+
+            StreamReader f1 = new StreamReader(pathfilename1, System.Text.Encoding.Default);
+            StreamReader f2 = new StreamReader(pathfilename2, System.Text.Encoding.Default);
+
+            string[] resultat = new string[] { "adipiscing", "dolor", "consectetur", "elit", "ipsum" };
+
+            llResultat.AddRange(resultat);
+
+            // L'ordre de les paraules amb les mateixes repeticions és imprevissible, caldria un proces que fes aquesta ordenació
+            // L'ordre de les úniques sí que es respecte
+            CollectionAssert.AreEquivalent(llResultat, biblioteca.paraulesMesRepetidesFile(ref f1, ref f2));
+        }
+
+        [TestMethod()]
+        public void ParaulesFileTest()
+        {
+            filename1="file1.txt";
+
+            pathfilename1=Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, string.Concat(@"..\..\..\", filename1));
+
+            //Debug.WriteLine(pathFile1);
+
+            StreamReader f1 = new StreamReader(pathfilename1, System.Text.Encoding.Default);
+
+            frase1 = "Lorem ipsum dolor sit amet consectetur adipiscing elit";
+
+            Assert.AreEqual(frase1, biblioteca.ParaulesFile(ref f1));
+        }
+    }
+}
